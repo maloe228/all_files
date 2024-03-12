49a8093e16487a88b2f0cef0afd8ed3f8166b11b
maloe228 <149201142+maloe228@users.noreply.github.com>
2024-03-12 23:50:42 +0300

    1

---
+++ 11.py
@@ -0,0 +1,44 @@
+from random import *
+N = 1000000
+i = 0
+d = [randint(1, 100) for n in range(N)]
+print(d)
+
+# print(sorted(d)) сортировка выбором
+
+while i < N - 1:
+	m = i
+	j = i + 1
+	while j < N:
+		if d[j] < d[m]:
+			m = j
+		j += 1
+	d[i], d[m] = d[m], d[i]
+	i += 1
+print(d)
+
+
+#пузырьки
+
+def bubble_sort(x):
+    for i in range(0,len(x)-1): 
+        for j in range(len(x)-1): 
+            if(x[j]>x[j+1]):  
+                x[j], x[j+1] = x[j+1], x[j]
+    return x
+ 
+list1 = [5, 3, 8, 6, 7, 2] 
+print(list1) 
+
+print(bubble_sort(list1)) 
+
+
+#пузырьки упростить
+
+x = [5, 3, 8, 6, 7, 2] 
+print(x)
+for i in range(0,len(x)-1):
+  for j in range(len(x)-1):
+    if(x[j]>x[j+1]):
+      x[j], x[j+1] = x[j+1], x[j]
+print(x)
